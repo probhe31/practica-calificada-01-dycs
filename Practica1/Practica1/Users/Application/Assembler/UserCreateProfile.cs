5553e2dd275e6c06121618e4571b90f67edda548
probhe31 <bhpaucar@gmail.com>
2018-10-03 21:52:37 -0500

    update profile

--- Practica1/Practica1/Users/Application/Assembler/UserCreateProfile.cs
+++ Practica1/Practica1/Users/Application/Assembler/UserCreateProfile.cs
@@ -1,4 +1,7 @@
 ﻿using AutoMapper;
+using Practica1.Companies.Domain.Entity;
+using Practica1.Users.Application.Dto;
+using Practica1.Users.Domain.Entity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +11,19 @@ namespace Practica1.Users.Application.Assembler
 {
     public class UserCreateProfile : Profile
     {
+
+        public UserCreateProfile()
+        {
+            CreateMap<UserCreateDto, User>()
+                .ForMember(
+                 dest => dest.Company,
+                    opts => opts.MapFrom(
+                        src => new Company
+                        {
+                            Id = src.CompanyId
+                        }
+                    )
+                ).ReverseMap();
+        }
     }
 }
